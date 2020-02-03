f0410b313a35472a5872596c7878823782868587
Fiarcherry <fiarchersschannel@gmail.com>
2020-02-03 16:56:16 +0300

    Добавлены 3 поля и функция selectOne

--- src/database/notification.js
+++ src/database/notification.js
@@ -2,25 +2,39 @@ import {executeSqlCommand} from "./executeCommand";
 
 export  function selectAllNotification() {
     executeSqlCommand(
-        'select * from notification',
-        [],
-        async (_, { rows: { _array } }) => {
-            await this.setState({
-                notes: _array,
-                refreshing: false
-            });
+    'select * from notification',
+    [],
+    async (_, { rows: { _array } }) => {
+        await this.setState({
+            notes: _array,
+            refreshing: false
         });
+    });
+}
+
+export  function selectOneNotification(id) {
+    executeSqlCommand(
+    'select * from notification where id = ' + id,
+    [],
+    async (_, { rows: { _array } }) => {
+        await this.setState({
+            notes: _array,
+            refreshing: false
+        });
+    });
 }
 
 export function createNotification() {
     executeSqlCommand(
         'create table if not exists notification (' +
         'id integer primary key autoincrement not null, ' +
-        'day integer, ' +
-        'month integer, ' +
+        'day text, ' +
+        'month text, ' +
         'year integer, ' +
         'hour integer, ' +
-        'minute integer',
+        'minute integer' +
+        'noteId integer' +
+        'foreign key(noteId) references note(id))',
         []
     );
 }
